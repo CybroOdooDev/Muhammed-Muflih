3e7ed5c6d34a69cf47b5af195e048bba3077ca7e
muflih <muflihanu60@gmail.com>
2026-06-04 17:39:31 +0530

    [add]advanced_excel_reports,pos_order_question

---
+++ pos_order_question/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <pos_order_question>
+
+#### 04.12.2025
+#### Version 19.0.1.0.0
+#### ADD
+- Initial commit for POS Order Questions
---
+++ pos_order_question/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from . import test_pos_order_line
+from . import test_pos_order_question
+from . import test_pos_session
+from . import test_product_template
