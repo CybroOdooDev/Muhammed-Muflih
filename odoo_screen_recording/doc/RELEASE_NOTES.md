f1f84744745047680d7d22358183b9a5e3752828
muflih <muflihanu60@gmail.com>
2026-06-04 17:55:56 +0530

    [add]odoo_screen_record

---
+++ odoo_screen_recording/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <odoo_screen_recording>
+
+#### 19.04.2025
+#### Version 18.0.1.0.0
+#### ADD
+- Initial commit for Odoo Screen Recording
---
+++ odoo_screen_recording/static/src/js/video_preview_widget.js
@@ -0,0 +1,23 @@
+/** @odoo-module **/
+import { registry } from "@web/core/registry";
+import { Component, onMounted , useState} from "@odoo/owl";
+//video widget
+let target;
+class videoWidget extends Component {
+    static template = 'VideoWidget'
+    setup() {
+        onMounted(this.mount);
+        super.setup();
+        this.state = useState({
+           value:false
+        });
+    }
+    mount() {
+        console.log('mounting video',this)
+        document.querySelector('source').setAttribute('src', this.props.record.data[this.props.name]);
+    }
+}
+export const VideoWidget = {
+    component: videoWidget
+}
+registry.category("fields").add("videoWidget", VideoWidget);
