df57a094369f95fd3968cbd89617c8c2ae537170
muflih <muflihanu60@gmail.com>
2026-06-29 17:56:22 +0530

    [ADD] media_repositort

---
+++ media_repository/__manifest__.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2026-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Muhammed Muflih c(odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Media Repository',
+    'version': '18.0.1.0.0',
+    'category': 'Tools',
+    'summary': 'Centralized location for storing, organizing, searching, and managing media assets',
+    'description':'The Media Repository module provides a centralized platform for storing, organizing, and managing media assets within Odoo. Users can upload files, categorize them, search efficiently.',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'license': 'LGPL-3',
+    'depends': ['base', 'mail'],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'data': [
+        'security/media_repository_groups.xml',
+        'security/ir.model.access.csv',
+        'security/media_repository_record_rules.xml',
+        'views/media_asset_views.xml',
+        'views/media_category_views.xml',
+        'views/media_type_dashboard_views.xml',
+    ],
+'assets': {
+   'web.assets_backend': [
+       'media_repository/static/src/js/dashboard.js',
+       'media_repository/static/src/xml/dashboard.xml',
+   ],
+},
+}
---
+++ media_repository/doc/RELEASE_NOTES.md
@@ -0,0 +1,8 @@
+## Module <hr_payroll_community>
+
+#### 29.06.2026
+#### Version 19.0.1.0.0
+#### ADD
+- Initial commit for Odoo19 media repository
+
+
